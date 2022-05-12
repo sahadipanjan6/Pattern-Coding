f89254c2f5bcd80ed5884e292b249b4fd2ace56f
Dipanjan Saha <sahadipanjan6@gmail.com>
2022-05-12 10:14:58 +0530

    Add files via upload

---
+++ pattern21.c
@@ -0,0 +1,25 @@
+/*
+	E D C B A
+	E D C B
+	E D C
+	E D
+	E
+
+*/
+
+#include<stdio.h>
+
+void main()
+{
+	int i, j = 0;
+	char ch;
+	
+	for(i=5; i>=1; i--)
+	{
+		ch = 'E';
+		for(j=1; j<=i; j++)
+			printf("%c ", ch--);
+			
+		printf("\n");
+	}
+}
